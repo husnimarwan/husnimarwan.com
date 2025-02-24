0433c5be935cc624e9c5904d0e96d1210017aa13
husnimarwan <56395929+husnimarwan@users.noreply.github.com>
2025-02-24 17:10:21 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const contactBtn = document.getElementById("contactBtn");
+    
+    contactBtn.addEventListener("click", function () {
+        alert("Thank you for reaching out! I will get back to you soon.");
+    });
+});
